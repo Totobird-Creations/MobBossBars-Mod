5eb1941e74db97cd51ce0ba6c31d9785221f9e71
Totobird <>
2023-06-21 12:49:54 -0400

    Updated to 1.20.1; Fixed fabric.mod.json;

--- src/main/java/net/totobirdcreations/mobbossbars/mixin/EnderDragonFightMixin.java
+++ src/main/java/net/totobirdcreations/mobbossbars/mixin/EnderDragonFightMixin.java
@@ -1,8 +1,8 @@
 package net.totobirdcreations.mobbossbars.mixin;
 
 import net.minecraft.entity.boss.dragon.EnderDragonFight;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.totobirdcreations.mobbossbars.util.IServerBossBarMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +15,8 @@ public class EnderDragonFightMixin {
 
     private final EnderDragonFight self = (EnderDragonFight)(Object) this;
 
-    @Inject(method = "<init>", at = @At(value = "RETURN"))
-    private void init(ServerWorld world, long gatewaysSeed, NbtCompound nbt, CallbackInfo ci) {
+    @Inject(method = "<init>(Lnet/minecraft/server/world/ServerWorld;JLnet/minecraft/entity/boss/dragon/EnderDragonFight$Data;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "RETURN"))
+    private void init(ServerWorld world, long gatewaysSeed, EnderDragonFight.Data data, BlockPos origin, CallbackInfo ci) {
         ((IServerBossBarMixin) self.bossBar).setIsDragonFight(true);
     }
 
